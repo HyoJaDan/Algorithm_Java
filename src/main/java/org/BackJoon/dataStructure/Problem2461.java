3862dba2a9b868083bf8ecd0678080fa8da4ebe2
전성호 <kopasd99@gmail.com>
2024-07-25 23:21:33 +0900

    2024 07 25

---
+++ src/main/java/org/BackJoon/dataStructure/Problem2461.java
@@ -0,0 +1,34 @@
+package org.BackJoon.dataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Problem2461 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int arr[][] = new int[n][m];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i=0;i<n;i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                pq.add(arr[i][j]);
+            }
+        }
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek());
+            pq.remove();
+
+        }
+    }
+}
--- src/main/java/org/example/PublicTemperature.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public class PublicTemperature {
-    public int temperature;
-}
