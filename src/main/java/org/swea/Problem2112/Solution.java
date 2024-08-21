c308d8e9430a545b2be13fd30e897a9301d63f9e
전성호 <kopasd99@gmail.com>
2024-08-21 23:16:09 +0900

    2112 6782

--- src/main/java/org/swea/Problem2112/Solution.java
+++ src/main/java/org/swea/Problem2112/Solution.java
@@ -1,102 +1,89 @@
 package main.java.org.swea.Problem2112;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Glass{
-	int level;
-	boolean ZeroOrOne;
-	
-	public Glass(int level, boolean ZeroOrOne) {
-		this.level=level;
-		this.ZeroOrOne=ZeroOrOne;
-	}
-}
-
 public class Solution {
-	private static Glass[][] map;
-	private static int T,N,M,K;
-	private static int[] path;
-	private static boolean[] visited;
-	
-	public static void main(String[] args)throws IOException{
+	static int D, W, K, minCount;
+	static int[][] film;
+	static int[][] backupFilm;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		T = Integer.parseInt(br.readLine());
-		
-		for(int TC=1; TC<=T; TC++) {
-			init(br, st);
-			
-			for(int i=1;i<=N;i++) {
-				dfs(0,0,i);
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			D = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+
+			film = new int[D][W];
+			backupFilm = new int[D][W];
+
+			for (int i = 0; i < D; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < W; j++) {
+					film[i][j] = Integer.parseInt(st.nextToken());
+					backupFilm[i][j] = film[i][j];
+				}
 			}
+
+			minCount = Integer.MAX_VALUE;
+			dfs(0, 0);
+			System.out.println("#" + t + " " + minCount);
 		}
 	}
-	public static void init(BufferedReader br, StringTokenizer st) throws IOException{
-		st = new StringTokenizer(br.readLine());
-		
-		N= Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		map = new Glass[N+1][M];
-		path = new int[N];
-		visited = new boolean[N];
-		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<M;j++) {
-				int inputData = Integer.parseInt(st.nextToken());
-				if(inputData==1)
-					map[i][j] = new Glass(1,true);
-				else map[i][j] = new Glass(1,false);
-			}
-		}
-		for(int j=0;j<M;j++) {
-			map[N][j] = new Glass(1,true);
-		}
-		for(int j=0;j<M;j++) {
-			int maxLevel=0;
-			for(int i=0; i<N; i++) {
-				int currentLevel=1,k=i;
-				while(k+1 < N && map[k+1][j].ZeroOrOne == map[i][j].ZeroOrOne) {
-					currentLevel++; k++;
-				}
-				while(i<=k) {
-					map[i++][j].level=currentLevel;
-				}
-				i--;
-				if(maxLevel < currentLevel) maxLevel = currentLevel;
-			}
-			map[N][j].level=maxLevel;
-		}
-		for(int i=0;i<=N;i++) {
-			for(int j=0;j<M;j++) {
-				System.out.print(map[i][j].level+" ");
-			}
-			System.out.println();
-		}
-	}
-	public static void dfs(int level, int now, int maxLevel) {
-		if(level==maxLevel) {
-			run();
-			for(int i=0;i<maxLevel;i++)
-				System.out.print(path[i]+" ");
-			System.out.println();
+
+	static void dfs(int depth, int count) {
+		if (checkFilm()) {
+			minCount = Math.min(minCount, count);
 			return;
 		}
-		
-		for(int i=now;i<N;i++) {
-			if(visited[i]) continue;
-			
-			visited[i] = true;
-			path[level] = i;
-			dfs(level+1, i, maxLevel);
-			path[level] = 0;
-			visited[i] = false;
+		if (depth == D || count >= minCount) return;
+
+		dfs(depth + 1, count); // 현재 막에 약품 투입 없이 다음으로 넘어감
+
+		applyMedicine(depth, 0); // A 약품 투입
+		dfs(depth + 1, count + 1);
+		restoreFilm(depth); // 원래 상태로 복구
+
+		applyMedicine(depth, 1); // B 약품 투입
+		dfs(depth + 1, count + 1);
+		restoreFilm(depth); // 원래 상태로 복구
+	}
+
+	static boolean checkFilm() {
+		if(K==1) return true;
+		for (int j = 0; j < W; j++) {
+			boolean passed = false;
+			int maxCount = 1;
+			for (int i = 1; i < D; i++) {
+				if (film[i][j] == film[i - 1][j]) {
+					maxCount++;
+					if (maxCount >= K) {
+						passed = true;
+						break;
+					}
+				} else {
+					maxCount = 1;
+				}
+			}
+			if (!passed) return false;
+		}
+		return true;
+	}
+
+	static void applyMedicine(int depth, int type) {
+		for (int j = 0; j < W; j++) {
+			film[depth][j] = type;
 		}
 	}
-	public static void run() {
-		
+
+	static void restoreFilm(int depth) {
+		for (int j = 0; j < W; j++) {
+			film[depth][j] = backupFilm[depth][j];
+		}
 	}
 }
---
+++ src/main/java/org/swea/Problem6782/Solution.java
@@ -0,0 +1,39 @@
+package org.swea.Problem6782;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        int T, cnt;
+        long N, temp;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; ++t) {
+            cnt = 0;
+            N = Long.parseLong(br.readLine());
+            while (true) {
+                if (N == 2)
+                    break;
+                else if (N == 1) // N이 1이면 1 증가 후 종료
+                {
+                    cnt += 1;
+                    break;
+                } else {
+                    temp = (long) (Math.sqrt(N));
+                    if (temp * temp == N) {
+                        ++cnt;
+                        N = temp;
+                    } else {
+//                        cnt += ((temp + 1) * (temp + 1) - N);
+//                        ++cnt;
+//                        N = temp + 1;
+                        cnt++;
+                        N++;
+                    }
+                }
+            }
+            System.out.println("#" + t + " " + cnt);
+        }
+    }
+}
